c0a7ba75f86348fc265884f9a3e3418004c4247d
Mark Martin <assplecake@gmail.com>
2015-10-14 20:45:24 -0700

    pull an unencrypted session object out of the opsee session header instead of authorization header

--- test/opsee/middleware/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns opsee-middleware.core-test
-  (:require [clojure.test :refer :all]
-            [opsee-middleware.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
