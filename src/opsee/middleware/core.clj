2de748e83703b013d7c37bd630b0481400c324ea
Dan Compton <dan@opsee.co>
2015-10-23 16:33:41 -0700

    add try-let macro

--- src/opsee/middleware/core.clj
+++ src/opsee/middleware/core.clj
@@ -111,6 +111,37 @@
      `(let ~(vec (apply concat (map (fn [[k v]] [v k]) deshadower)))
         ~(if-and-let* bindings then-clause else-clause deshadower)))))
 
+(defmacro try-let
+  "A combination of try and let such that exceptions thrown in the binding or
+   body can be handled by catch clauses in the body, and all bindings are
+   available to the catch and finally clauses. If an exception is thrown while
+   evaluating a binding value, it and all subsequent binding values will be nil.
+   Example:
+   (try-let [x (f a)
+             y (f b)]
+     (g x y)
+     (catch Exception e (println a b x y e)))"
+  {:arglists '([[bindings*] exprs* catch-clauses* finally-clause?])}
+  [bindings & exprs]
+  (when-not (even? (count bindings))
+    (throw (IllegalArgumentException. "try-let requires an even number of forms in binding vector")))
+  (let [names  (take-nth 2 bindings)
+        values (take-nth 2 (next bindings))
+        ex     (gensym "ex__")]
+    `(let [~ex nil
+           ~@(interleave names (repeat nil))
+           ~@(interleave
+               (map vector names (repeat ex))
+               (for [v values]
+                 `(if ~ex
+                    [nil ~ex]
+                    (try [~v nil]
+                         (catch Throwable ~ex [nil ~ex])))))]
+       (try
+         (when ~ex (throw ~ex))
+         ~@exprs))))
+
+
 (defn slurp-bytes
   "Slurp the bytes from a slurpable thing"
   [x]
