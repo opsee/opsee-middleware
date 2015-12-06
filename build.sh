63b53334ebe2c77f7a15e72b443c4d4fb97159cd
Dan Compton <dan@opsee.co>
2015-12-06 10:40:05 -0800

    build the aws proto as well

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java \
         --proto_path=/usr/local/include \
         --java_out=src --grpc-java_out=src --proto_path=resources/proto resources/proto/checker.proto
+protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java \
+        --proto_path=/usr/local/include \
+        --java_out=src --grpc-java_out=src --proto_path=resources/proto resources/proto/aws.proto
