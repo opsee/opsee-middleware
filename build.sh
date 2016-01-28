0ae3bd5bb00372257ceb2b4027ad2e184925f2db
Mark Martin <assplecake@gmail.com>
2016-01-27 17:29:02 -0800

    add gogo protobuf descriptor

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java \
         --proto_path=/usr/local/include \
+        --java_out=src --grpc-java_out=src --proto_path=resources/proto /usr/local/include/github.com/gogo/protobuf/gogoproto/gogo.proto
+protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java \
+        --proto_path=/usr/local/include \
         --java_out=src --grpc-java_out=src --proto_path=resources/proto resources/proto/checker.proto
 protoc --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java \
         --proto_path=/usr/local/include \
