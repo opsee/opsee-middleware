f93c456551712667fe9dd29ff69601f5f2fa612c
CI <bots@opsee.co>
2016-04-26 16:28:40 +0000

    Version 0.1.31-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject co.opsee/opsee-middleware "0.1.30"
+(defproject co.opsee/opsee-middleware "0.1.31-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
